fe13192214a4b3e5c392ba87d0319a232c44a7c0
kartikey <kartikkey0414@gmail.com>
2025-04-10 16:49:27 +0530

    Added type check for super admin password

--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -1,7 +1,9 @@
 import { User } from './model';
 import { hashPassword } from '../../utils/hash';
-import dotenv from 'dotenv';
-dotenv.config();
+import { env } from 'node:process';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
 export const createDefaultSuperAdmin = async () => {
     const existing = await User.findOne({ role: 'super-admin' });
     if (existing) {
@@ -12,7 +14,7 @@ export const createDefaultSuperAdmin = async () => {
     const user = new User({
       name: 'Super Admin',
       email: process.env.SUPER_ADMIN_EMAIL,
-      password: await hashPassword(process.env.SUPER_ADMIN_PASSWORD),
+      password: await hashPassword(process.env.SUPER_ADMIN_PASSWORD as string),
       role: 'super-admin'
     });
   
